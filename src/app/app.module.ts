7337c8d107d9c11518b294bff6c2acac6106ae1b
Arthus15 <diablo100_1@hotmail.com>
2019-06-30 17:09:27 +0200

    Add style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
+import { MaterialModule } from 'src/material-module';
 
 
 @NgModule({
@@ -14,10 +15,7 @@ import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/mat
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatToolbarModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
