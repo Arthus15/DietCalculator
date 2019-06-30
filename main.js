cff779e40164b8e660bee2c9db2f777f7706219c
Jaio Blanco <diablo100_1@hotmail.com>
2019-06-30 15:01:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+    //Create the browser windows
+    win = new BrowserWindow({
+        width: 800,
+        height: 800,
+        backgroundcolor: '#ffffff',
+        icon: `file://${__dirname}/dist/assets/logo.png`
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    //uncomment below to open the devtools.
+    win.webContents.openDevTools()
+
+    win.on('closed', function() {
+        win = null
+    })
+}
+
+//Create window on electron initialization
+app.on('ready', createWindow)
+
+//Quit when all windows are closed
+app.on('window-all-closed', function(){
+
+    //On macOs specific close process
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if(win == null){
+        createWindow()
+    }
+})
